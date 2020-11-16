0bd5d5fe7bea09f41cf4b51d2293a11f5551f3ba
GermanoMacieira <germanomacieira@hotmail.com>
2020-11-16 13:51:33 -0300

    Atualização do projeto em 16/Nov/2020 13:50

--- siteAlgorimos/server/index.js
+++ siteAlgorimos/server/index.js
@@ -1,44 +1,56 @@
-const http = require('http');
-const fs = require('fs');
-const hostname = 'localhost';
-const port = 3000;
-const responseFile = (res, file, contentType) => {
-    fs.readFile(file, null, (err, data) => {
-        if(err){
-            res.statusCode = 500;
-            res.end(err.message);
-        }else{
-            res.statusCode = 200;
-            res.setHeader('Content-Type', contentType);
-            res.write(data);
-            res.end();
-        }
-    });
-}
+var express = require('express');           // Importamos o Express para a nossa aplicação poder utilizar;
+var bodyParser = require('body-parser');    // Referência para a requisição da biblioteca(módulo) do Body-parser;
+var funcoes = require('./algoritmos');
+var app = express();                        // Instanciamos uma aplicação Express. A partir de agora podemos utilizar os comandos que o Express nos fornece;
+var path = require('path');                 // Pacote embutido no node. Trabalha com os caminho do SO. Concatena caminhos.
 
-const server = http.createServer((req, res) => {
-    switch(req.url){
-        case '/' || '/index.html':
-            responseFile(res, '../client/html/index.html', 'text/html');
-            break;
-        case '/index.css':
-            responseFile(res, '../client/css/index.css', 'text/css');
-            break;
-        case '/isPrime.html':
-            responseFile(res, '../client/html/isPrime.html', 'text/html');
-            break;
-        case '/isPrime.html':
-            responseFile(res, '../client/html/isPrime.html', 'text/html');
-            break;
-        case '/quickSort.html':
-            responseFile(res, '../client/html/quickSort.html', 'text/html');
-            break;
-        default:
-            res.statusCode = 404;
-            res.end('Página nâo encontrada.');
-    }
+var port = 3000;
+app.use(bodyParser.json());            // Está recebendo os dados como json. O bodyParser intercepta as requisições. Essa biblioteca ajudará na leitura dos dados que vamos enviar via POST.
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('public'));          // Indicando a pasta public.
+
+app.set('view engine', 'ejs');      // Setar o motor (template) de views usado.
+app.set('views', path.join(__dirname, 'views')); // Caminho da pasta views.
+
+app.get('/', function(req, res) {
+    const parametro = {
+        titulo: 'Algorítimos Fundamentais'      // O mesmo "titulo" do arquivo "index.ejs"
+    };
+    res.render('index', parametro);
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+/*
+app.get('/', function(req, res) {         // Configuração de uma url (Só para testar)
+    res.send('Algorítimos Fundamentais');
+});
+*/
+
+// 1-url Contador
+app.post('/contador', function(req, res) {
+
+    var body = req.body;
+    var a = parseFloat(body.a);
+    var resultado = funcoes.contador(a);       // body.a é a variável enviada para a aplicação. 
+    res.send(`O somatório dos números anteriores a ${a} é: ${resultado}`);  // Finalização da requisição;
+});
+
+// 2-url Fibonacci
+
+
+// 3-url MDC
+
+
+// 4-url Primo
+
+
+// 5-url Quicksort
+
+
+// 6-url Somatorio
+
+
+
+app.listen(port, function() {                                           // Indicação da porta.
+    console.log('Servidor escutando na porta http://localhost:${port}/');
 });
